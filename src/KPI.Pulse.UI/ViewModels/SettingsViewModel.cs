ed9a05aeb56f4442b561091ebb62aa1f3e38ded8
alexander.yurtaev <alexander.yurtaev@gmail.com>
2026-02-18 12:09:35 +0300

    Implement navigation in the Settings.

--- src/KPI.Pulse.UI/ViewModels/SettingsViewModel.cs
+++ src/KPI.Pulse.UI/ViewModels/SettingsViewModel.cs
@@ -1,5 +1,10 @@
-﻿using System;
+﻿using KPI.Pulse.UI.Models;
+using KPI.Pulse.UI.Services;
 using ReactiveUI;
+using Splat;
+using System;
+using System.Linq;
+using System.Reactive;
 
 namespace KPI.Pulse.UI.ViewModels
 {
@@ -7,10 +12,26 @@ namespace KPI.Pulse.UI.ViewModels
     {
         public string? UrlPathSegment { get; } = Guid.NewGuid().ToString().Substring(0, 5);
         public IScreen HostScreen { get; }
+        public NavItem RootNavItem { get; set; }
+        public ReactiveCommand<NavItem, Unit> NavigateToNavItemCommand { get; }
 
         public SettingsViewModel(IScreen screen)
         {
             HostScreen = screen;
+
+            var uiService = Locator.Current.GetService<IUiService>() ??
+                            throw new InvalidOperationException(nameof(IUiService));
+
+            var navItems = uiService.GetNavItems(HostScreen);
+            RootNavItem = navItems.First();
+
+            NavigateToNavItemCommand = ReactiveCommand.Create<NavItem>(_ =>
+            {
+                if (HostScreen is IInteractionViewModel interactionVm)
+                {
+                    interactionVm.NavigateToNavItem.Handle(RootNavItem.Id).Subscribe();
+                }
+            });
         }
-    }
+}
 }
